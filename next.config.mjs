596f6fc428ad86ecb2e5f851361286d2bf251fad
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-06-20 17:04:39 +0530

    feat: add outputFileTracing configuration to next.config.mjs and generate process output files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracing: process.env.VERCEL === '1',
   images: {
     remotePatterns: [
       {
